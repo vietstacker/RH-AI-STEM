a90a9e353cd7590ebf0c6db7b174804d8208d436
Tutj <tuantuluong@gmail.com>
2020-01-07 23:01:46 +0800

    Adding necessary scripts

---
+++ indexing.py
@@ -0,0 +1,11 @@
+import os
+
+path = 'google_images/images/'
+counter = 1
+
+for f in os.listdir(path):
+    suffix = f.split('.')[-1]
+    if suffix == 'jpg' or suffix == 'png':
+        new = '{}.{}'.format(str(counter), suffix)
+        os.rename(path + f, path + new)
+        counter = int(counter) + 1
